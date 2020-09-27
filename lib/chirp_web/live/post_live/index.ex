6b6ae3f5c8129aaaf3b968b402ce46f5510c7148
Hyun Ahn <codepawn@icloud.com>
2020-09-28 01:21:41 +0900

    add update socket part

--- lib/chirp_web/live/post_live/index.ex
+++ lib/chirp_web/live/post_live/index.ex
@@ -7,7 +7,7 @@ defmodule ChirpWeb.PostLive.Index do
   @impl true
   def mount(_params, _session, socket) do
     if connected?(socket), do: Timeline.subscribe()
-    {:ok, assign(socket, :posts, list_posts())}
+    {:ok, assign(socket, :posts, fetch_posts()), temporary_assigns: [posts: []]}
   end
 
   @impl true
@@ -38,7 +38,7 @@ defmodule ChirpWeb.PostLive.Index do
     post = Timeline.get_post!(id)
     {:ok, _} = Timeline.delete_post(post)
 
-    {:noreply, assign(socket, :posts, list_posts())}
+    {:noreply, assign(socket, :posts, fetch_posts())}
   end
 
   @impl true
@@ -50,7 +50,7 @@ defmodule ChirpWeb.PostLive.Index do
     {:noreply, update(socket, :posts, fn posts -> [post | posts] end)}
   end
 
-  defp list_posts do
+  defp fetch_posts do
     Timeline.list_posts()
   end
 end
