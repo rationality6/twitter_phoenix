10b958b2386e0e7bc759027bf239f6a3157cc996
Hyun Ahn <codepawn@icloud.com>
2020-09-29 01:05:34 +0900

    set url for socket handshake

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,8 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :chirp, ChirpWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  # url: [host: "example.com", port: 80],
+  url: [host: "127.0.0.1", port: 4001],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
