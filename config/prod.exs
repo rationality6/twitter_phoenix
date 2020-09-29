755ad46bb10265dc98e2bd90ec6c858cf5dfd8a3
Hyun Ahn <codepawn@icloud.com>
2020-09-29 11:37:06 +0900

    fix port

--- config/prod.exs
+++ config/prod.exs
@@ -10,11 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :chirp, ChirpWeb.Endpoint,
-  # url: [host: "example.com", port: 80],
-
-  # http: [port: {:system, "PORT"}], # Possibly not needed, but doesn't hurt
-  url: [host: "https://turquoise-angry-chrysalis.gigalixirapp.com", port: 443],
-
+  url: [host: nil, port: 443],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
@@ -57,15 +53,3 @@ config :logger, level: :info
 # Finally import the config/prod.secret.exs which loads secrets
 # and configuration from environment variables.
 import_config "prod.secret.exs"
-
-# config :gigalixir_getting_started, GigalixirGettingStartedWeb.Endpoint,
-#   http: [port: {:system, "PORT"}], # Possibly not needed, but doesn't hurt
-#   url: [host: "${APP_NAME}.gigalixirapp.com", port: 443],
-#   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
-#   server: true
-
-# config :gigalixir_getting_started, GigalixirGettingStarted.Repo,
-#   adapter: Ecto.Adapters.Postgres,
-#   url: System.get_env("DATABASE_URL"),
-#   ssl: true,
-#   pool_size: 2 # Free tier db only allows 4 connections. Rolling deploys need pool_size*(n+1) connections where n is the number of app replicas.
